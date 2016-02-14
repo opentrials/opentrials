6dd263c116825688d30dce6bb9160bb5d9a18e2b
Vitor Baptista <vitor@vitorbaptista.com>
2016-02-14 14:08:41 +0000

    Get API's URL to mock from ENV variables

--- test/common.js
+++ test/common.js
@@ -1,1 +1,3 @@
+require('dotenv').config();
+
 global.apiServer = require('./fixtures/api');
--- test/fixtures/api/index.js
+++ test/fixtures/api/index.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const nock = require('nock');
 
+const swaggerApiUrl = process.env.SWAGGER_API_URL;
+const url = swaggerApiUrl.slice(0, swaggerApiUrl.length - 'swagger.yaml'.length);
 const swaggerPath = path.join(__dirname, 'swagger.yaml');
 
-module.exports = nock('http://localhost:10010')
+module.exports = nock(url)
   .get('/swagger.yaml')
   .reply(200, fs.readFileSync(swaggerPath, 'utf-8'));
