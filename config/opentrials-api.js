a477ff3a2eb2e96a1a2f74095c013ac0dd28368a
Vitor Baptista <vitor@vitorbaptista.com>
2016-02-15 12:56:00 +0000

    Forgot to add versioning (/v1) to config/opentrials-api.js

--- config/opentrials-api.js
+++ config/opentrials-api.js
@@ -1,6 +1,6 @@
 const Swagger = require('swagger-client');
 
 module.exports = new Swagger({
-  url: process.env.SWAGGER_API_URL || 'http://localhost:10010/swagger.yaml',
+  url: process.env.SWAGGER_API_URL || 'http://localhost:10010/v1/swagger.yaml',
   usePromise: true,
 });
