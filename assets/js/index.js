9f3fb705ff1150ece485ec283629f4e00e6f64e3
Vitor Baptista <vitor@vitorbaptista.com>
2016-03-25 17:21:27 +0000

    [#40] Indenting JS file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
   // mobile menu
-   $("#menu").mmenu({
-      offCanvas: {
-        pageSelector: ".page",
-        position  : "right"
-      }
+  $("#menu").mmenu({
+    offCanvas: {
+      pageSelector: ".page",
+      position: "right",
+    },
    }, {
-         // configuration
-         classNames: {
-            selected: "active"
-         }
+     // configuration
+     classNames: {
+       selected: "active",
+     },
   });
 
-  //home page
-  $( ".toggle-advanced" ).click(function() {
-    $( ".advanced" ).slideToggle( "slow", function() {
-      $( ".home" ).toggleClass( "advanced-search" )
+  // home page
+  $(".toggle-advanced").click(function() {
+    $(".advanced").slideToggle("slow", function() {
+      $(".home").toggleClass("advanced-search")
     });
   });
-
 });
