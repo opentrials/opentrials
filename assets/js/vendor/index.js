775732af57e2bf20569db4b82d0dd01f65242a6a
Vitor Baptista <vitor@vitorbaptista.com>
2016-03-29 20:28:42 +0100

    [#40] Fix input[date] polyfill
    Instead of better-dateinput-polyfill, we're now using the
    nodep-date-input-polyfill. It's much smaller and has no dependencies, although
    by default it is quite ugly.

--- assets/js/vendor/index.js
+++ assets/js/vendor/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-require('./better-dom.js');
-require('./better-i18n-plugin.js');
-require('./better-dateinput-polyfill.js');
+require('../../../node_modules/nodep-date-input-polyfill/nodep-date-input-polyfill.dist.js');
 
 require('../../../node_modules/jquery.mmenu/dist/js/jquery.mmenu.min.js');
