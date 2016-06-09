236d63867aa263bf70735e12afd8c1d3023b99e0
Vitor Baptista <vitor@vitorbaptista.com>
2016-06-09 12:17:53 +0100

    [#203] Fix UI bug when a trial had no registration_date
    The issue is that the `moment` library, when called with `undefined`, returns
    the current date. We avoid this behavior by checking for `undefined` in
    `views/filters/format-date.js`. I've also changed the UI to display a message
    when we have no registration date.

--- test/views/filters/format-date.js
+++ test/views/filters/format-date.js
@@ -1,8 +1,15 @@
+'use strict';
+
+const should = require('should');
 const formatDate = require('../../../views/filters/format-date');
 
 describe('formatDate', () => {
   it('formats date as DD/MMM/YYYY', () => {
     const date = new Date('2016/04/20');
     formatDate(date).should.eql('20/Apr/2016');
+  });
+
+  it('returns undefined when called with undefined', () => {
+    should(formatDate(undefined)).be.undefined();
   });
 });
--- views/filters/format-date.js
+++ views/filters/format-date.js
@@ -1,7 +1,9 @@
 const moment = require('moment');
 
 function formatDate(date) {
-  return moment(date).format('DD/MMM/YYYY');
+  if (date) {
+    return moment(date).format('DD/MMM/YYYY');
+  }
 }
 
 module.exports = formatDate;
