852b9d1b1075c83129e5d8f20810f54eaa564bef
Vitor Baptista <vitor@vitorbaptista.com>
2016-11-16 12:17:35 +0200

    [#501] Add test for FDA's index handler and ignore coverage on Glue config
    opentrials/opentrials#501

---
+++ test/lib/modules/fda/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const should = require('should');
+
+
+describe('FDA Index Handler', () => {
+  let server;
+
+  before(() => getFDAServer().then((_server) => server = _server));
+
+  describe('GET /', () => {
+    it('redirects to /search', () => (
+      server.inject('/')
+        .then((response) => {
+          should(response.statusCode).eql(302);
+          should(response.headers.location).eql('/search');
+        })
+    ));
+  });
+});
