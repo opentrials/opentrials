2d71ae21a2e1ff37d3a3695ef264f9511a9bf5a0
Vitor Baptista <vitor@vitorbaptista.com>
2016-11-29 11:30:11 -0500

    [#574] Extract FAQ to a separate page, outside About
    opentrials/opentrials#574

---
+++ test/lib/modules/fda/faq.js
@@ -0,0 +1,31 @@
+'use strict';
+
+describe('FDA About Handler', () => {
+  let server;
+
+  before(() => getFDAServer().then((_server) => server = _server));
+
+  describe('GET /faq', () => {
+    it('is successful', () => (
+      server.inject('/faq')
+        .then((response) => {
+          response.statusCode.should.equal(200);
+        })
+    ));
+
+    it('uses the "fda/faq" template', () => (
+      server.inject('/faq')
+        .then((response) => {
+          response.request.response.source.template.should.equal('fda/faq')
+        })
+    ));
+
+    it('sets the title', () => (
+      server.inject('/faq')
+        .then((response) => {
+          const context = response.request.response.source.context;
+          context.should.have.property('title');
+        })
+    ));
+  });
+});
